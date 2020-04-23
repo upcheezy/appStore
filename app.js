c07a4f794e690e8c9abb2cf4af5d9359044ac11a
Matthew Upchurch <upcheezy@gmail.com>
2020-04-22 21:31:12 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const morgan = require('morgan');
+const storeapps = require('./playstore.js')
+const cors = require('cors')
+
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(cors())
+
+app.get('/apps', (req, res) => {
+    const {
+        search = "",
+        sort,
+        genre
+    } = req.query;
+
+    if (genre) {
+        if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
+            return res
+                .status(400)
+                .send('sort must include the given dropdown values')
+        }
+    }
+
+    if (sort) {
+        if (!['rating', 'app'].includes(sort)) {
+            return res
+                .status(400)
+                .send('sort must be one of title or rank')
+        }
+    }
+
+    let results = storeapps
+        .filter(apps =>
+            apps
+            .App
+            .toLowerCase()
+            .includes(search.toLowerCase()));
+
+    if (sort) {
+        results
+            .sort((a, b) => {
+                return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+            });
+    }
+
+    console.log(results);
+
+    res.json(results);
+})
+
+app.listen(8001, () => {
+    console.log('Server started on port 8001');
+});
